b28cf231e2a173b35cafd2dc4a3f10b8639c769d
Jalon Duncan <duncanjalon@gmail.com>
2021-09-21 13:09:29 -0500

    init

---
+++ DogGo/Controllers/OwnersController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DogGo.Repositories;
+using DogGo.Models;
+
+namespace DogGo.Controllers
+{
+    public class OwnersController : Controller
+    {
+        // GET: OwnersController
+        public ActionResult Index()
+        {
+            List<Owner> owners = _ownerRepo.GetAllOwners();
+
+            return View(walkers);
+        }
+
+        // GET: OwnersController/Details/5
+        public ActionResult Details(int id)
+        {
+            Owner owners = _OownerRepo.GetWalkerById(id);
+            if (owners == null)
+            {
+                return NotFound();
+            }
+
+            return View(owner);
+        }
+
+        // GET: OwnersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OwnersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OwnersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: OwnersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OwnersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: OwnersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
